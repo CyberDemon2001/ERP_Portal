a389c9081f539e337a3543059f1ffc6d4fe1485f
CyberDemon2001 <vaibhavtyagi254@gmail.com>
2025-03-22 15:13:05 +0530

    pages

---
+++ client/src/pages/student/timetable.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TimeTable = () => {
+    return (
+        <div>
+            <h1>Attendance Page</h1>
+            <p>This is the attendance page.</p>
+        </div>
+    );
+};
+
+export default TimeTable;
